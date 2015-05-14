7780b7938a866b96322de093853f41fa3ed0a35a
JacobDowns <jacob.downs@umontana.edu>
2015-05-14 15:53:45 -0600

    cleaned some stuff up

--- coupled.py
+++ coupled.py
@@ -36,9 +36,7 @@ def fy2(t, ys):
   y2 = ys[1]
   return (-C * y2 + D * y1 * y2)
 
-#pc_solver = PCSolver([y1, y2], [fy1, fy2], 0.0, 0.2)
 pc_solver = PCSolver([y1, y2], [fy1, fy2], 0.0, 0.025, verbose = True)
-#rk_solver = RKSolve([y1, y2], [fy1, fy2], 0.0, dt = 0.1, dt_max = 0.1, tol = 1e-6, output = True)
 # Final time step
 T = 40.0
 # Lists for plotting solutions
@@ -47,16 +45,16 @@ ts3 = np.linspace(0, T, 100)
 ys1 = [3.0]
 ys2 = [5.0]
 
-t0 = time.time()
 # Solve using predictor corrector
 while pc_solver.t < T:
   pc_solver.step() 
   ts.append(pc_solver.t)
   ys1.append(y1.vector()[0])
   ys2.append(y2.vector()[0])
-t1 = time.time()
 
-
-plt.plot(ts, ys1,'ro-')
-plt.plot(ts, ys2,'go-')
+plt.title("Coupled Equations")
+plt.plot(ts, ys1,'ro-', label = "y1")
+plt.plot(ts, ys2,'go-', label = "y2")
+plt.xlabel("t")
+plt.legend(loc = 2)
 plt.show()
